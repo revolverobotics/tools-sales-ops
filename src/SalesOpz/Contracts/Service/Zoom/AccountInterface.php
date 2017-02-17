c9f0062c97b10181f6ad0b95daaedf8e5dcd36ea
Tim <tim@revolverobotics.com>
2017-02-17 00:22:00 -0800

    Update AccountInterface.php

--- src/SalesOpz/Contracts/Service/Zoom/AccountInterface.php
+++ src/SalesOpz/Contracts/Service/Zoom/AccountInterface.php
@@ -14,101 +14,82 @@ interface AccountInterace extends ServiceInterface
     /**
      * Create a sub account.
      *
-     * @param  string $email
-     * @param  string $firstName
-     * @param  string $lastName
-     * @param  string $password
-     * @param  mixed  $options     [...] See Zoom documentation for all other available parameters
+     * @param  array  $input
      * @return array  JsonResponse {id, owner_id, owner_email, created_at}
      */
-    public function createSubAccount($email, $firstName, $lastName, $password, ...$options);
+    public function createSubAccount(array $input);
 
     /**
      * Update a sub account.
      *
-     * @param  string $accountId
-     * @param  mixed  $options     [...] See Zoom documentation for all other available parameters
+     * @param  array  $input
      * @return array  JsonResponse {id, updated_at}
      */
-    public function updateSubAccount($accountId, $options = null);
+    public function updateSubAccount(array $input);
 
     /**
      * Delete a sub account.
      *
-     * @param  string $accountId
+     * @param  array  $input
      * @return array  JsonResponse {id, deleted_at}
      */
-    public function deleteSubAccount($accountId);
+    public function deleteSubAccount(array $input);
 
     /**
      * List all the sub accounts under the master account.
      *
+     * @param  array  $input
      * @return array  JsonResponse {page_count, page_number, page_size, total_records, subAccounts: {id, owner_email, created_at}}
      */
-    public function listSubAccount();
+    public function listSubAccount(array $input);
 
     /**
      * Get a sub account.
      *
-     * @param  string $accountId
+     * @param  array  $input
      * @return array  JsonResponse {id, owner_id, owner_email, created_at}
      */
-    public function getSubAccount($accountId);
+    public function getSubAccount(array $input);
 
     /**
      * Subscribe a plan for a sub account. Only works for free sub accounts,
      * to be paid for by the master account (No existing plans).
      *
-     * @param  string $email
-     * @param  string $firstName
-     * @param  string $lastName
-     * @param  string $phoneNumber
-     * @param  string $address
-     * @param  string $city
-     * @param  string $country
-     * @param  string $state
-     * @param  string $zip
-     * @param  string $planBase {type, hosts}
-     *                          ['type'  => ['trial', 'monthly', 'yearly', 'business_monthly', 'business_yearly', 'edu20', 'zroom_monthly', 'zroom_yearly']]
-     *                          ['hosts' => (int)]
-     * @param  mixed  $options  [...] See Zoom documentation for all other available parameters
+     * @param  array  $input
      * @return JsonResponse     {...} See Zoom documentation for more details
      */
-    public function subscribePlan($email, $firstName, $lastName, $phoneNumber, $address, $city, $country, $state, $zip, $planBase, ...$options);
+    public function subscribePlan(array $input);
 
     /**
      * Add a plan to a sub account. Only works for sub accounts that already
      * have existing plans and are paid for by the master account.
      *
-     * @param string $accountId
-     * @param string $plan      {type, hosts} See Zoom documentation for more details
+     * @param  array  $input
+     * @return JsonResponse     {...} See Zoom documentation for more details
      */
-    public function addPlan($accountId, $plan);
+    public function addPlan(array $input);
 
     /**
      * Update a plan for a paid sub account.
      *
-     * @param  string  $accountId
-     * @param  integer $type      1 means Base Plan, 2 means Additional Plan.
-     * @param  string  $plan      [...] See Zoom documentation for all available plans
+     * @param  array  $input
      * @return JsonResponse       {id, updated_at}
      */
-    public function updatePlan($accountId, $type, $plan);
+    public function updatePlan(array $input);
 
     /**
      * Get plan information for a sub account.
      *
-     * @param  string  $accountId
+     * @param  array  $input
      * @return JsonResponse       {...} See Zoom documentation for more details
      */
-    public function getPlan($accountId);
+    public function getPlan(array $input);
 
     /**
      * Update the billing information for a sub account.
      *
-     * @param  string $accountId
-     * @param  mixed  $options   [...] See Zoom documentation for all available parameters
+     * @param  array  $input
      * @return JsonResponse      {id, updated_at}
      */
-    public function updateBilling($accountId, ...$options);
+    public function updateBilling(array $input);
 }
